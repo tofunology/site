7f0678b4500651e02be29b98816927bf94b7e4e6
Gitefu <out.grow.1028@gmail.com>
2021-10-22 01:04:32 +0900

    homeにアニメーション追加

---
+++ js/intro-head_animation.js
@@ -0,0 +1,21 @@
+// ヘッダーを取得
+const introHead = document.getElementById("introHead");
+
+// 現在地を示す変数を定義
+let nextImage =1;
+let count =0;
+
+let imgArray;
+let preImg= new Image();
+
+const changeHeadImg = () => {
+    if(introHead){
+        introHead.style.backgroundImage = "url( "+imgArray[nextImage] + ")";
+        nextImage = (nextImage == imgArray.length-1)?0:nextImage+1;
+        //プレロード
+        preImg.src = imgArray[nextImage];
+    }
+};
+
+
+setInterval(changeHeadImg, 8000);
